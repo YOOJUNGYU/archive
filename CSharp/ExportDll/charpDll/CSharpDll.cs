04e816c3d11aded7cc0f72a1dcfc083a6a160c5d
yoojungyu <yoojungyu@ubware.co.kr>
2021-10-28 10:47:34 +0900

    * ExportDll settings 루트 변경

---
+++ CSharp/ExportDll/charpDll/CSharpDll.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CSharpDll
+{
+    public class CSharpDll
+    {
+        [ExportDll("Test", CallingConvention.StdCall)]
+        public static string Test(string str)
+            => $"받은 내용: {str}";
+    }
+}
