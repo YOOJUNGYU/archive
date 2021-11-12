3b71e1b8d893605ae4fe67f9a32a95860c3df4d3
yoojungyu <yoojungyu@ubware.co.kr>
2021-11-12 12:36:31 +0900

    ...

--- CSharp/WinFormWebView2/WinFormWebView2/Test2/FormTest2.cs
+++ CSharp/WinFormWebView2/WinFormWebView2/Test2/FormTest2.cs
@@ -8,11 +8,14 @@ namespace WinFormWebView2.Test2
         public FormTest2()
         {
             InitializeComponent();
+
+            // Test2.html 불러오기
             webView.Source = new Uri($@"{Application.StartupPath}\Test2\Html\Test2.html");
         }
 
         private void webView_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
         {
+            // Test2.html과 bridge 연결
             webView.CoreWebView2.AddHostObjectToScript("bridge", new Bridge());
         }
     }
