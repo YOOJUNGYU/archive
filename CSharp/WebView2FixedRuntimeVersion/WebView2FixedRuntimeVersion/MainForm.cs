2e05dbe81f1a34d3a56f989710c9063fe78f9fcf
YOOJUNGYU <yoojungyu@ubcare.co.kr>
2023-05-31 09:19:12 +0900

    WebView2FixedRuntimeVersion 적용

---
+++ CSharp/WebView2FixedRuntimeVersion/WebView2FixedRuntimeVersion/MainForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+
+namespace WebView2FixedRuntimeVersion
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeWebView2();
+        }
+
+        private async void InitializeWebView2()
+        {
+            var webView = new WebView2
+            {
+                CreationProperties = new CoreWebView2CreationProperties
+                {
+                    BrowserExecutableFolder =
+                        $"{Application.StartupPath}\\Microsoft.WebView2.FixedVersionRuntime.112.0.1722.68.x64"
+                }
+            };
+            ((ISupportInitialize) webView).EndInit();
+            await webView.EnsureCoreWebView2Async();
+            Controls.Add(webView);
+            webView.Dock = DockStyle.Fill;
+            webView.NavigationCompleted += webView_NavigationCompleted;
+            webView.Source = new Uri($"{Application.StartupPath}\\hello.html");
+        }
+
+        private void webView_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+        }
+    }
+}
